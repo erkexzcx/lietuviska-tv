e89420d6030e3cf58efe79c3bd3caeba3f95b690
Erikas Rudinskas <erikmnkl@gmail.com>
2019-12-10 18:15:24 +0200

    Fix typo

--- build.sh
+++ build.sh
@@ -6,20 +6,20 @@ if ! [ -d "src" ]; then
 	exit 1
 fi
 
-# Prepare output directory:
-mkdir -p dist
+rm -rf dist
 
 # Build Linux binaries:
 env GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o "dist/lietuviskatv_linux_i386" src/*.go             # Linux i386
 env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o "dist/lietuviskatv_linux_x86_64" src/*.go         # Linux 64bit
-
-# Build Linux ARM binaries:
-env GOOS=freebsd GOARCH=amd64 go build -ldflags="-s -w" -o "dist/lietuviskatv_freebsd_x86_64" src/*.go     # FreeBSD 64bit
-
-# Build freebsd binary:
 env GOOS=linux GOARCH=arm GOARM=5 go build -ldflags="-s -w" -o "dist/lietuviskatv_linux_arm" src/*.go      # Linux armv5/armel/arm (it also works on armv6)
 env GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-s -w" -o "dist/lietuviskatv_linux_armhf" src/*.go    # Linux armv7/armhf
 env GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o "dist/lietuviskatv_linux_aarch64" src/*.go        # Linux armv8/aarch64
+
+# Build FreeBSD binary:
+env GOOS=freebsd GOARCH=amd64 go build -ldflags="-s -w" -o "dist/lietuviskatv_freebsd_x86_64" src/*.go     # FreeBSD 64bit
+
+# Build MacOS binary:
+env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o "dist/lietuviskatv_darwin_x86_64" src/*.go       # Darwin 64bit
 
 # Build Windows binaries (oh God whyyy):
 env GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o "dist/lietuviskatv_windows_i386.exe" src/*.go     # Windows 32bit
