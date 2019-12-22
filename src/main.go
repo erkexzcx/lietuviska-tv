8038ec1877e1d1172aa489f62c5c404326f71618
Erikas Rudinskas <erikmnkl@gmail.com>
2019-12-22 19:57:35 +0200

    Print available app's URLs on startup

--- src/main.go
+++ src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -29,6 +32,31 @@ func main() {
 
 	log.Println("Started!")
 
+	ips, err := getAvailableURLs()
+	if err == nil {
+		fmt.Println("\nUse below IP addresses to reach M3U playlist:")
+		for _, link := range ips {
+			fmt.Println("\t" + link)
+		}
+		fmt.Println()
+	}
+
 	log.Fatal(http.ListenAndServe(":8989", nil))
 
+}
+
+func getAvailableURLs() ([]string, error) {
+	var IPs []string
+	addresses, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range addresses {
+		address := v.String()
+		if strings.Contains(address, "::") {
+			continue
+		}
+		IPs = append(IPs, "http://"+strings.SplitN(address, "/", 2)[0]+":8989/iptv")
+	}
+	return IPs, nil
 }
